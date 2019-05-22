115432e46f150b542ce2fcadec81b778d7575db3
Alex Herridge <alherridge@gmail.com>
2019-05-22 14:58:34 -0400

    Base Setup

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,12 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: 'AIzaSyDFcLZ24jLC-1wCHGib6N4m-4IwKO5OrY0',
+  authDomain: 'libre-bib.firebaseapp.com',
+  databaseURL: 'https://libre-bib.firebaseio.com',
+  projectId: 'libre-bib',
+  storageBucket: 'libre-bib.appspot.com',
+  messagingSenderId: '762004849458',
+  appId: '1:762004849458:web:c2a949daa8d1d2e6',
 };
 
 class Firebase {
@@ -34,32 +35,10 @@ class Firebase {
 
   // *** Auth API ***
 
-  doCreateUserWithEmailAndPassword = (email, password) =>
-    this.auth.createUserWithEmailAndPassword(email, password);
-
-  doSignInWithEmailAndPassword = (email, password) =>
-    this.auth.signInWithEmailAndPassword(email, password);
-
   doSignInWithGoogle = () =>
     this.auth.signInWithPopup(this.googleProvider);
 
-  doSignInWithFacebook = () =>
-    this.auth.signInWithPopup(this.facebookProvider);
-
-  doSignInWithTwitter = () =>
-    this.auth.signInWithPopup(this.twitterProvider);
-
   doSignOut = () => this.auth.signOut();
-
-  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
-
-  doSendEmailVerification = () =>
-    this.auth.currentUser.sendEmailVerification({
-      url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
-    });
-
-  doPasswordUpdate = password =>
-    this.auth.currentUser.updatePassword(password);
 
   // *** Merge Auth and DB User API *** //
 
