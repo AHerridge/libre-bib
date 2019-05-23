9361f9035a2598f678eda9a62e3e828f39e37d94
Alex Herridge <alherridge@gmail.com>
2019-05-22 21:06:49 -0400

    Switched to signout icon

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
-  <button type="button" onClick={firebase.doSignOut}>
-    Sign Out
-  </button>
+  <a href="#" onClick={firebase.doSignOut}>
+    <img width="40" height="40" src="https://cdn1.iconfinder.com/data/icons/materia-arrows-symbols-vol-3/24/018_128_arrow_exit_logout-512.png"></img>
+  </a>
 );
 
 export default withFirebase(SignOutButton);
